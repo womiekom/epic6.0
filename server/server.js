437f50dff60e4d904a19819843a35920e6856582
womiekom <159969864+womiekom@users.noreply.github.com>
2026-03-19 18:06:41 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ app.post("/generate-ticket", async (req, res) => {
             })
         }
 
-        // 🔥 SEND EMAIL HERE
+        // SEND EMAIL
         await sendTicketsEmail(
             order.email,
             order.name,
@@ -86,4 +86,6 @@ app.post("/generate-ticket", async (req, res) => {
 
 })
 
-app.listen(3000, () => console.log("Server running on port 3000"))
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
