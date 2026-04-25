7ebdec20c8582e4452ba64625fd7f39452a86fd8
womiekom <raiyan.mufidgaming@gmail.com>
2026-04-26 03:05:25 +0700

    attendance test

--- client/attendance/attendance.js
+++ client/attendance/attendance.js
@@ -2,25 +2,15 @@ const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby78mDXpowhTXbFv4qz-
 
 const form = document.getElementById("absenForm");
 const statusText = document.getElementById("status");
-const button = form.querySelector("button");
-
-let isSubmitting = false;
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
-
-    if (isSubmitting) return; // 🔥 block spam
 
     const name = document.getElementById("name").value;
     const division = document.getElementById("division").value;
     const otp = document.getElementById("code").value;
 
-    isSubmitting = true;
-    button.disabled = true;
-    button.innerText = "Submitting...";
-
     statusText.innerText = "Validating...";
-    statusText.style.color = "white";
 
     try {
         const res = await fetch(SCRIPT_URL, {
@@ -33,26 +23,14 @@ form.addEventListener("submit", async (e) => {
         if (data.success) {
             statusText.innerText = "Absen berhasil";
             statusText.style.color = "green";
-
-            button.innerText = "Success ✔";
-            button.style.background = "green";
-
             form.reset();
         } else {
             statusText.innerText = data.message;
             statusText.style.color = "red";
-
-            isSubmitting = false;
-            button.disabled = false;
-            button.innerText = "Absen";
         }
 
     } catch (err) {
         statusText.innerText = "Server error";
         statusText.style.color = "red";
-
-        isSubmitting = false;
-        button.disabled = false;
-        button.innerText = "Absen";
     }
 });
