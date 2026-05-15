982441a34809f9b52c85806342050b3c33374b69
womiekom <159969864+womiekom@users.noreply.github.com>
2026-05-16 06:11:37 +0700

    Update otp.js

--- client/attendance/otp.js
+++ client/attendance/otp.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwpVdy6mbfQva7WPwCL-R0m-uGKUygD8yN6JayDn9yyCM3Alge7U0_ekVtzzpqYNxBO/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyS5456uBJOftBJwyn-9-8sX506rN9iEAgbREVzjGlpn0si-O_7imXCeNSjhVj3rbXj/exec";
 
 const otpEl = document.getElementById("otp");
 const statusEl = document.getElementById("status");
